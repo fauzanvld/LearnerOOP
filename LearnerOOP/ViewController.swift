d32ce89447734405c74fcea0d72c550657f8793b
Fauzan Aldwinavaldhy Wiratnoputra <fauzanvld@gmail.com>
2019-05-15 14:19:12 +0700

    passes CD10 & CD17 LO

--- LearnerOOP/ViewController.swift
+++ LearnerOOP/ViewController.swift
@@ -10,10 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var learnerNameLabel: UILabel!
+    @IBOutlet weak var learnerAgeLabel: UILabel!
+    @IBOutlet weak var learnerGenderLabel: UILabel!
+    
+    var learnerInstance: LearnerModel? // copy Learner model
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        learnerInstance = LearnerModel(nameLearner: "Jan", ageLearner: 21, genderLearner: "Pria", imageProfileLearner: "")
+        
+        updateUI()
     }
 
+    func updateUI() {
+        if let instance = learnerInstance {
+            learnerNameLabel.text = instance.name
+            learnerAgeLabel.text = "\(instance.age)"
+            learnerGenderLabel.text = instance.gender
+        }
+        
+     
+    }
 
+    @IBAction func increaseAgeButtonClicked(_ sender: UIButton) {
+        if let instance = learnerInstance {
+            instance.increaseAge()
+            updateUI()
+        }
+    }
+    
 }
 
